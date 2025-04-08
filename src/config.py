1c3bfa5fdf3a223a310a466980860d415aea77a9
daanifm <daniel.foronda657@comunidadunir.net>
2025-04-08 17:20:46 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,6 +4,6 @@ from openai import OpenAI
 def init_client():
     client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key="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",
+    api_key="OpenRouter ApiKey",
     )
     return client
